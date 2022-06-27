8ffc4153b2e8f88fe598a1eae474df50c6e9ba23
picklemon <108268930+picklemon@users.noreply.github.com>
2022-06-27 15:29:30 +0530

    Create calc.py

---
+++ calc.py
@@ -0,0 +1,89 @@
+import time #IMPORTS TIME FROM OS#
+import math #IMPORTS MATH MODULE
+
+#ADDTION STARTS HERE#
+
+time.sleep(1)
+print("\nA D D I T I O N")
+time.sleep(1)
+b=float(input("\nEnter first number "))
+time.sleep(1)
+c=float(input("Enter second number "))
+d=b + c
+time.sleep(1)
+print("\nSum is " + str(d))
+
+#SUBTRACTION STARTS HERE#
+
+time.sleep(1)
+print("\nS U B T R A C T I O N")
+time.sleep(1)
+e=float(input("\nEnter first number "))
+time.sleep(1)
+f=float(input("Enter second number "))
+g=e - f
+time.sleep(1)
+print("\nDifference is " + str(g))
+
+#MULTIPLICATIONS STARTS HERE#
+
+time.sleep(1)
+print("\nM U L T I P L I C A T I O N")
+time.sleep(1)
+h=float(input("\nEnter first number "))
+time.sleep(1)
+i=float(input("Enter second number "))
+j=h * i
+time.sleep(1)
+print("\nProduct is " + str(j))
+
+#DIVISION STARTS HERE#
+
+time.sleep(1)
+print("\nD I V I S I O N")
+time.sleep(1)
+k=float(input("\nEnter first number "))
+time.sleep(1)
+l=float(input("Enter second number "))
+m=k / l
+time.sleep(1)
+print("\nQuotient is " + str(m))
+
+#SQUARE STARTS HERE#
+
+time.sleep(1)
+print("\nS Q U A R E")
+time.sleep(1)
+n=float(input("\nEnter any number "))
+o= n * n
+time.sleep(1)
+print("\nSquare is " + str(o))
+
+#CUBE STARTS HERE#
+
+time.sleep(1)
+print("\nC U B E")
+time.sleep(1)
+p=float(input("\nEnter any number "))
+q= p * p * p
+time.sleep(1)
+print("\nCube is " + str(q))
+
+#SQUARE ROOT STARTS FROM HERE#
+
+time.sleep(1)
+print("\nS Q U A R E - R O O T")
+time.sleep(1)
+u=float(input("\nEnter a number "))
+v=math.sqrt(u)
+print("\nSquare root is " + str(v))
+
+#CUBE ROOT STARTS FROM HERE#
+
+time.sleep(1)
+print("\nC U B E - R O O T")
+time.sleep(1)
+r=float(input("\nEnter any number "))
+s=r ** (1./3.)
+print("\nCube root is " + str(s))
+time.sleep(3)
