339bb4f273d1d36f88155afdae900962fba0cdca
Pickemon <108268930+picklemon@users.noreply.github.com>
2022-06-30 12:43:34 +0530

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,151 +1,166 @@
 import time
 import math
 
-message= "Do you want to do anything else? (y/n)"
-tqmess= "\nThank you for using calculator. This program will exit in 5 seconds."
-
-print('''Welcome to Calculator
-This calculator can calculate the following things -\n
-[-] Addition 
+print('''Welcome to Calculator. This Calculator can Calculate the following things -
+\n[-] Addition
 [-] Subtraction
 [-] Division
 [-] Multiplication
-[-] Square
-[-] Square root
-[-] Cube
-[-] Cube root\n
+[-] Square and Square Root
+[-] Cube and Cube Root\n
 ''')
 def main():
     time.sleep(1)
-    a=input('''\nWhat do you want to do-\n
+    a = input(''' What do you want to do?
     a - Addition
     b - Subtraction
     c - Division
     d - Multiplication
     e - Square
-    f - Square root
+    f - Square Root
     g - Cube
-    h - Cube root
-    \nEnter the corresponding alphabet here ''')
+    h - Cube Root
+    \nEnter the corresponding alphabet here - 
+    ''')
     if a == "a":
-        print("\nA D D I T I O N")
-        time.sleep(0.5)
-        b=float(input("\nEnter first number "))
-        c=float(input("Enter second number "))
-        d= b + c
-        time.sleep(1)
-        print("\nSum is ", d)
-        r=input(message)
-        if r == "y" or r == "Y":
-            main()
-        else:
-            print(tqmess)
-            time.sleep(5)
-    
-    elif a == "b":
-        print("\nS U B T R A C T I O N")
+        print("\nA D D I T I O N\n")
         time.sleep(0.5)
         b=float(input("Enter first number "))
-        c=float(input("Enter second number "))
-        d= b - c
-        time.sleep(1)
-        print("\nDifference is ", d)
-        r=input(message)
-        if r == "y" or r == "Y":
-            main()
-        else:
-            print(tqmess)
-            time.sleep(5)
-    
-    elif a == "c":
-        print("\nD I V I S I O N")
-        time.sleep(0.5)
-        b=float(input("\nEnter first number "))
-        c=float(input("Enter second number "))
-        d= b / c
-        time.sleep(1)
-        print("\nQuotient is ", d)
-        r=input(message)
-        if r == "y" or r == "Y":
-            main()
-        else:
-            print(tqmess)
-            time.sleep(5)
+        b=b+float(input("Enter second number "))
+        print("\nSum is ", b)
 
-    elif a == "d":
-        print("\nM U L T I P L I C A T I O N")
-        time.sleep(0.5)
-        b=float(input("\nEnter first number "))
-        c=float(input("Enter second number "))
-        d= b * c
-        time.sleep(1)
-        print("\nProduct is ", d)
-        r=input(message)
-        if r == "y" or r == "Y":
+        r = input("Do you want to do anything else? (y/n)")
+        if r == "y":
+            time.sleep(0.5)
             main()
+        elif r == "n":
+            input("Thank you for using Calculator. Press Enter Key to exit this program...")
         else:
-            print(tqmess)
-            time.sleep(5)
+            print("Invalid alphabet...")
+            main()
+
+    elif a == "b":
+        print("\nS U B T R A C T I O N\n")
+        time.sleep(0.5)
+        b=float(input("Enter first number "))
+        b=b-float(input("Enter second number "))
+        print("\nDifference is ", b)
+
+        r = input("Do you want to do anything else? (y/n)")
+        if r == "y":
+            time.sleep(0.5)
+            main()
+        elif r == "n":
+            input("Thank you for using Calculator. Press Enter Key to exit this program...")
+        else:
+            print("Invalid alphabet...")
+            main()
+
+    elif a == "c":
+        print("\nD I V I S I O N\n")
+        time.sleep(0.5)
+        b=float(input("Enter first number "))
+        b=b/float(input("Enter second number "))
+        print("\nQuotient is ", b)
+
+        r = input("Do you want to do anything else? (y/n)")
+        if r == "y":
+            time.sleep(0.5)
+            main()
+        elif r == "n":
+            input("Thank you for using Calculator. Press Enter Key to exit this program...")
+        else:
+            print("Invalid alphabet...")
+            main()
+        
+    elif a == "d":
+        print("\nM U L T I P L I C A T I O N\n")
+        time.sleep(0.5)
+        b=float(input("Enter first number "))
+        b=b*float(input("Enter second number "))
+        print("\nProduct is ", b)
+
+        r = input("Do you want to do anything else? (y/n)")
+        if r == "y":
+            time.sleep(0.5)
+            main()
+        elif r == "n":
+            input("Thank you for using Calculator. Press Enter Key to exit this program...")
+        else:
+            print("Invalid alphabet...")
+            main()
 
     elif a == "e":
-        print("\nS U A R E")
+        print("\nS Q U A R E\n")
         time.sleep(0.5)
-        b=float(input("\nEnter a number "))
-        c= b * b
-        time.sleep(1)
-        print("\nSquare is ", c)
-        r=input(message)
-        if r == "y" or r == "Y":
-            main()
-        else:
-            print(tqmess)
-            time.sleep(5)
+        b=float(input("Enter any number "))
+        b=b * b
+        print("\nSquare is ", b)
 
+        r = input("Do you want to do anything else? (y/n)")
+        if r == "y":
+            time.sleep(0.5)
+            main()
+        elif r == "n":
+            input("Thank you for using Calculator. Press Enter Key to exit this program...")
+        else:
+            print("Invalid alphabet...")
+            main()
+    
     elif a == "f":
         print("\nS Q U A R E - R O O T\n")
         time.sleep(0.5)
-        b=float(input("\nEnter a number "))
-        c= math.sqrt(b)
-        time.sleep(1)
-        print("\nSquare root is ", c)
-        r=input(message)
-        if r == "y" or r == "Y":
+        b=float(input("Enter any number "))
+        b=math.sqrt(b)
+        print("\nSquare root is ", b)
+
+        r = input("Do you want to do anything else? (y/n)")
+        if r == "y":
+            time.sleep(0.5)
             main()
+        elif r == "n":
+            input("Thank you for using Calculator. Press Enter Key to exit this program...")
         else:
-            print(tqmess)
-            time.sleep(5)
-    
+            print("Invalid alphabet...")
+            main()
+
     elif a == "g":
         print("\nC U B E\n")
         time.sleep(0.5)
-        b=float(input("\nEnter any number "))
-        c= b * b * b
-        time.sleep(1)
-        print("\nCube is ", c)
-        r=input(message)
-        if r == "y" or r == "Y":
-            main()
-        else:
-            print(tqmess)
-            time.sleep(5)
+        b=float(input("Enter any number "))
+        b=b * b * b
+        print("\nCube is ", b)
 
+        r = input("Do you want to do anything else? (y/n)")
+        if r == "y":
+            time.sleep(0.5)
+            main()
+        elif r == "n":
+            input("Thank you for using Calculator. Press Enter Key to exit this program...")
+        else:
+            print("Invalid alphabet...")
+            main()
+        
     elif a == "h":
-        print("\nC U B E - R  O O T \n")
+        print("\nC U B E - R O O T\n")
         time.sleep(0.5)
-        b=float(input("\nEnter any number "))
-        c = b ** (1/3)
-        time.sleep(0.5)
-        print("\nCube root is ", c)
-        r=input(message)
-        if r == "y" or r == "Y":
-            main()
+        b=float(input("Enter any number "))
+        b=b ** (1/3)
+        print("\nCube root is ", b)
 
+        r = input("Do you want to do anything else? (y/n)")
+        if r == "y":
+            time.sleep(0.5)
+            main()
+        elif r == "n":
+            input("Thank you for using Calculator. Press Enter Key to exit this program...")
         else:
-            print(tqmess)
-            time.sleep(5)
+            print("Invalid alphabet...")
+            main()
 
     else:
-        print("\nInvalid alphabet.")
+        print("Invalid alphabet...")
+        time.sleep(0.5)
         main()
 
 main()
